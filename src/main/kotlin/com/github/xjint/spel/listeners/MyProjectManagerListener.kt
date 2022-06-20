f1781a15e622e3990299055d9d77c95e0a7be953
GitHub Action <action@github.com>
2022-06-20 06:18:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/xjint/spel/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/xjint/spel/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.xjint.spel.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.xjint.spel.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
